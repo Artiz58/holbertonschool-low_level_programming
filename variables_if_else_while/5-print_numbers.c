106aee2c1a344a2e459801c0cfdcf4105d8ccbad
Artiz58 <artiz58@gmail.com>
2023-10-16 14:45:54 -0400

    Numbers

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Prints the alphabet in lowercase.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+       int number;
+
+	for (number = '0'; number <= '10'; number++)
+	{
+		printf("%d\n");
+	}
+
+	return (0);
+}
