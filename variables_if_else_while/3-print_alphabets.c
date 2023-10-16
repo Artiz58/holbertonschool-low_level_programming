d257ffaf97e558c2407eede3e0106f3f9c179cb2
Artiz58 <artiz58@gmail.com>
2023-10-16 13:55:16 -0400

    alphABET

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Prints the alphabet in lowercase.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+        char abc;
+	char ABC;
+        for (abc = 'a'; abc <= 'z'; abc++)
+        {
+                putchar (abc);
+        }
+	while (ABC = 'A'; ABC <= 'Z'; ABC++)
+	{
+		putchar (ABC);
+	}	
+	putchar ('\n');
+
+        return (0);
+}
