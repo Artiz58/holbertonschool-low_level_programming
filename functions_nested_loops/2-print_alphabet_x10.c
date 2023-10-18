064a5577eabd850c22bcffe1f246731bf8b363e2
Artiz58 <artiz58@gmail.com>
2023-10-18 08:47:56 -0400

    10 x alphabet

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+	char abc;
+	int bucle = 10
+
+	for (i = 0; i < bucle; i++)
+	{
+		_putchar(abc);
+	}
+	_purchar('\n');
+}
