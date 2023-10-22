82b6d783906f8cca26c5caa4f4c9496b7cc8a13e
Artiz58 <artiz58@gmail.com>
2023-10-21 21:30:42 -0400

    Collaboration is multiplication

--- more_functions_nested_loops/2-mul.c
+++ more_functions_nested_loops/2-mul.c
@@ -2,11 +2,12 @@
 #include <stdio.h>
 
 /**
- *
- *
- *
- */
-
+*mul - Product of two integers
+*
+*@a: value for int a
+*@b: value for int b
+*Return: result, the multiplication a*b
+*/
 int mul(int a, int b)
 {
 	int c;
