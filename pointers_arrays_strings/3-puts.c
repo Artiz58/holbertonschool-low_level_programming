18ecbc5349ab45a0191ad5bfe60b8333b1dd4f60
Artiz58 <artiz58@gmail.com>
2023-10-23 17:25:24 -0400

    I do not fear computers. I fear the lack of them

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -3,14 +3,19 @@
 /**
  * _puts - prints a string to stdout
  * @str: pointer to the string to print
+ *
+ * Return: print string
  */
 void _puts(char *str)
 {
-	int i;
+	int i = 0;
+	char letter;
 
-	for (i = 0; str[i] != '\0'; i++)
+	while (str[i] != '\0')
 	{
-		_putchar(str[i]);
+		letter = str[i];
+		_putchar(letter);
+		i++;
 	}
-	_putchar('\n');
+_putchar('\n');
 }
