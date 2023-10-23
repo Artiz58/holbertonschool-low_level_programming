6342f05dc9ffd014c6901eb31d35c8bc22db6eaf
Artiz58 <artiz58@gmail.com>
2023-10-23 13:11:04 -0400

    This report, by its very length, defends itself against the risk of being read

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -2,14 +2,16 @@
 #include <stdio.h>
 
 /**
- * _strlen - 
- *
- */
+  * _strlen - Returns the length of a string
+  * @s: String to count
+  *
+  * Return: String length
+  */
 int _strlen(char *s)
 {
 	int c = 0;
 
-	for(; *s != '\0'; s++)
+	for (; *s != '\0'; s++)
 	{
 		c++;
 	}
