dba1ec2490ebb566231b223a86f34522fcc4b0ec
Artiz58 <artiz58@gmail.com>
2023-10-25 11:08:10 -0400

    Great leaders are willing to sacrifice the numbers to save the people. Poor leaders sacrifice the people to save the numbers

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,27 +1,38 @@
-#include <stdio.h>
 #include "main.h"
+#include <stdio.h>
 
-int _atoi(char *s) 
+/**
+ *_atoi - takes strings of numbers an puts them in a int
+ *
+ *@s: stores string
+ * Return: int value of string text
+ */
+int _atoi(char *s)
 {
-	int sign = 1;
-	int res = 0;
+	int j = 0, i = 0;
+	int signo = 1;
+	int result = 0;
 
-	while (*s == ' ')
+	while (s[i] != '\0')
 	{
-		s++;
+		i++;
 	}
-
-	if (*s == '+' || *s == '-')
+	i--;
+	for (j = 0; j <= i; j++)
 	{
-		sign = (*s == '+') ? 1 : -1;
-		s++;
+		if (s[j] == '-')
+		{
+			signo *= -1;
+		}
+		if ((s[j] >= '0' && s[j] <= '9'))
+		{
+			result = result * 10 + (s[j] - '0');
+			if (s[j + 1] < '0' || s[j + 1] > '9')
+			{
+				j = i + 1;
+			}
+		}
 	}
-
-	while (*s >= '0' && *s <= '9') {
-		res *= 10;
-		res += *s - '0';
-		s++;
-	}
-
-	return (res * sign);
+	result = result * signo;
+	return (result);
 }
