c1f4828d9b86030f15c7fbcee70490b31066574c
Artiz58 <artiz58@gmail.com>
2023-10-27 11:12:47 -0400

    strstr

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -1,31 +1,40 @@
 #include "main.h"
+#include <stddef.h>
 
 /**
  * _strstr - locates a substring.
- * @haystack: imput value
- * @needle: imput value
+ * @haystack: input value
+ * @needle: input value
  *
- * Return: haystack
+ * Return: pointer to the first
+ * occurrence of the substring, or NULL if not found
  */
 char *_strstr(char *haystack, char *needle)
 {
-	while (*haystack)
+	char *haystack_ptr = haystack;
+	char *needle_ptr = needle;
+
+	while (*haystack_ptr != '\0')
 	{
-	char *start = haystack;
-	char *sub = needle;
-
-	while (*haystack && *sub && *haystack == *sub)
-	{
-		haystack++;
-		sub++;
+		if (*haystack_ptr == *needle_ptr)
+		{
+			char *haystack_ptr_tmp = haystack_ptr + 1;
+			char *needle_ptr_tmp = needle_ptr + 1;
+			while (*haystack_ptr_tmp != '\0' && *needle_ptr_tmp != '\0')
+			{
+				if (*haystack_ptr_tmp != *needle_ptr_tmp)
+				{
+					break;
+				}
+				haystack_ptr_tmp++;
+				needle_ptr_tmp++;
+			}
+			if (*needle_ptr_tmp == '\0') 
+			{
+				return haystack_ptr;
+			}
+		}
+		haystack_ptr++;
 	}
-
-	if (!*sub)
-
-		return (start);
-
-	haystack = start + 1;
-	}
-
-return (haystack);
+	return NULL;
 }
