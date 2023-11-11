ea6c5cbd67809e36e692f3fdbb0d24cd2a5c6d0d
Artiz58 <artiz58@gmail.com>
2023-11-11 13:58:56 -0400

    A door is what a dog is perpetually on the wrong side of

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,41 +1,79 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include "dog.h"
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
- * new_dog - creates a new dog with given name, age, and owner
- * @name: name of the dog
- * @age: age of the dog
- * @owner: owner of the dog
+ * my_strdup - Custom string duplication function.
  *
- * Return: pointer to the new dog structure, or NULL if allocation fails
+ * @str: String to duplicate.
+ *
+ * Return: Pointer to the newly allocated
+ * duplicate string, or NULL if failure.
+ */
+
+char *my_strdup(char *str)
+{
+	char *dup_str;
+	int len = 0, i;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+
+	dup_str = malloc((len + 1) *
+			sizeof(char));
+
+	if (dup_str == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i <= len; i++)
+	{
+		dup_str[i] = str[i];
+	}
+
+	return (dup_str);
+}
+
+/**
+ * new_dog - Function that creates a new dog.
+ *
+ * @name: name of dog struct
+ * @age: age of dog struct
+ * @owner: owner of dog struct
+ *
+ * Return: new struct
  */
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *new_dog;
+	dog_t *new_dog = malloc(sizeof(dog_t));
 
-	new_dog = malloc(sizeof(dog_t));
 	if (new_dog == NULL)
-
-		return (NULL);
-
-	new_dog->name = strdup(name);
-	if (new_dog->name == NULL)
 	{
-		free(new_dog);
-
 		return (NULL);
 	}
 
-	new_dog->owner = strdup(owner);
-	if (new_dog->owner == NULL)
+	new_dog->name = my_strdup(name);
+	new_dog->owner = my_strdup(owner);
+
+	if (new_dog->name == NULL
+		|| new_dog->owner == NULL)
 	{
 		free(new_dog->name);
+		free(new_dog->owner);
 		free(new_dog);
 		return (NULL);
 	}
+
 	new_dog->age = age;
+
 	return (new_dog);
 }
