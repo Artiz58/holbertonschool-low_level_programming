a6a41a24ac9da74f9f6072748751599b8fab2997
Artiz58 <artiz58@gmail.com>
2023-12-05 21:10:22 -0400

    C - File I/O

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "main.h"
 /**
  * main - Copies the content of a file to another file.
@@ -7,47 +9,46 @@
  */
 int main(int argc, char *argv[])
 {
-	int fd_from, fd_to;
-	char buff[1024];
-	ssize_t br, bw;
+	int source, dest;
+	int num1 = 1024, num2 = 0;
+	char buf[1024];
 
 	if (argc != 3)
 	{
-		dprintf(2, "Usage: %s file_from file_to\n", basename(argv[0]));
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	fd_from = open(argv[1], O_RDONLY);
-	if (fd_from == -1)
+	source = open(argv[1], O_RDONLY);
+	if (source == -1)
 	{
-		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
-		dprintf(2, "Got: %d\n", 98);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	if (fd_to == -1)
+	dest = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR
+	| S_IRGRP | S_IWGRP | S_IROTH);
+	if (dest == -1)
 	{
-		dprintf(2, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
+	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+	close(source);
+	exit(99);
 	}
-	while ((br = read(fd_from, buff, sizeof(buff))) > 0)
+	while (num1 == 1024)
 	{
-		bw = write(fd_to, buff, br);
-		if (bw != br)
-		{
-			dprintf(2, "Error: Can't write to %s\n", argv[2]);
-			exit(99);
-		}
-	}
-	if (br < 0)
+		num1 = read(source, buf, 1024);
+	if (num1 == -1)
 	{
-		dprintf(2, "Error: Can't read from files %s\n", argv[1]);
-		dprintf(2, "Got: %d\n", 98);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	if (close(fd_from) == -1 || close(fd_to) == -1)
-	{
-		dprintf(2, "Error: Can't close fd\n");
-		exit(100);
+	num2 = write(dest, buf, num1);
+	if (num2 < num1)
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 	}
-	return (0);
+	if (close(source) == -1)
+	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", source), exit(100);
+
+	if (close(dest) == -1)
+	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", dest), exit(100);
+
+return (0);
 }
--- file_io/main.h
+++ file_io/main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
+#define BUF_SIZE 1024
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -12,6 +13,7 @@ size_t _strlen(const char *s);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+void print_error_and_exit(int code, const char *format, const char *arg);
 
 
 #endif
